52bd7f6ef830ffa9f1cbd3555bbabd48f6a64bc7
NotAlex575 <dapontealessandro89gmail.com>
2025-07-17 04:35:53 +0200

    importato script javascript che inserisce un container con 6 valori all'interno del html

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const teamContainer = document.getElementById("team-members");
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -37,5 +39,35 @@ const teamMembers = [
   }
 ];
 
+const renderCards = () =>{
+  for(let i = 0; i < teamMembers.length; i++){
+    let card = generateCard(teamMembers[i]);
+    teamContainer.innerHTML += card;
+  }
+}
 
+const generateCard = (teamMembers) => {
+  const{name, role, email, img} = teamMembers;
+
+  const memberCard = ` <div class="col-12 col-md-4">
+            <div class="card rounded-0 border-0">
+                <div class="w-20">
+                    <div class="card-img-top">
+                        <img src="./img/${img}" alt="" class="img-fluid">
+                    </div>
+                </div>}
+                <div class="w-80">
+                    <div class="card-body text-center">
+                        <h4>${name}</h4>
+                        <p>${role}</p>
+                        <p>${email}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`
+
+        return memberCard;
+}
+
+renderCards();
 
